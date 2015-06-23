52b98755a8b26fb50d90b7988ee8ee16053e5c11
wbond <will@wbond.net>
2015-06-23 09:11:35 -0400

    Update lint.py to automatically find .py files

--- lint.py
+++ lint.py
@@ -11,22 +11,13 @@ rc_path = os.path.join(cur_dir, './.pylintrc')
 
 print('Running pylint...')
 
-files = [
-    '__init__.py',
-    '_osx_ctypes.py',
-    '_osx_public_key.py',
-    '_osx_symmetric.py',
-    '_osx_util.py',
-    '_win_util.py',
-    'errors.py',
-    'kdf.py',
-    'keys.py',
-    'public_key.py',
-    'symmetric.py',
-    'util.py',
-]
+files = []
+for root, dirnames, filenames in os.walk('oscrypto/'):
+    for filename in filenames:
+        if not filename.endswith('.py'):
+            continue
+        files.append(os.path.join(root, filename))
 
-args = ['--rcfile=%s' % rc_path]
-args += ['oscrypto/' + f for f in files]
+args = ['--rcfile=%s' % rc_path] + files
 
 Run(args)
