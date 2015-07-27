7231b26c74be6f21b49e7c0d06142d6c4c45211d
wbond <will@wbond.net>
2015-07-27 09:49:45 -0400

    Fixed some bugs in integer serialization on Python 2.7

--- oscrypto/_int.py
+++ oscrypto/_int.py
@@ -11,12 +11,13 @@ Exports the following items:
 from __future__ import unicode_literals, division, absolute_import, print_function
 
 import sys
+import math
 
 
 # Python 2
 if sys.version_info <= (3,):
 
-    def int_to_bytes(value, signed=False):
+    def int_to_bytes(value, signed=False, width=None):
         """
         Converts an integer to a byte string
 
@@ -26,20 +27,39 @@ if sys.version_info <= (3,):
         :param signed:
             If the byte string should be encoded using two's complement
 
+        :param width:
+            None == auto, otherwise an integer of the byte width for the return
+            value
+
         :return:
             A byte string
         """
 
         # Handle negatives in two's complement
+        is_neg = False
         if signed and value < 0:
-            value = (~value) + 1
+            is_neg = True
+            bits = int(math.ceil(len('%x' % abs(value)) / 2.0) * 8)
+            value = (value + (1 << bits)) % (1 << bits)
 
         hex_str = '%x' % value
         if len(hex_str) & 1:
             hex_str = '0' + hex_str
+
         output = hex_str.decode('hex')
-        if signed and ord(output[0:1]) & 0x80:
+
+        if signed and not is_neg and ord(output[0:1]) & 0x80:
             output = b'\x00' + output
+
+        if width is not None:
+            if is_neg:
+                pad_char = b'\xFF'
+            else:
+                pad_char = b'\x00'
+            output = (pad_char * (width - len(output))) + output
+        elif is_neg and ord(output[0:1]) & 0x80 == 0:
+            output = b'\xFF' + output
+
         return output
 
     def int_from_bytes(value, signed=False):
@@ -71,7 +91,7 @@ if sys.version_info <= (3,):
 # Python 3
 else:
 
-    def int_to_bytes(value, signed=False):
+    def int_to_bytes(value, signed=False, width=None):
         """
         Converts an integer to a byte string
 
@@ -81,14 +101,22 @@ else:
         :param signed:
             If the byte string should be encoded using two's complement
 
+        :param width:
+            None == auto, otherwise an integer of the byte width for the return
+            value
+
         :return:
             A byte string
         """
 
-        result = value.to_bytes((value.bit_length() // 8) + 1, byteorder='big', signed=signed)
-        if not signed:
-            return result.lstrip(b'\x00')
-        return result
+        if width is None:
+            width_ = math.ceil(value.bit_length() / 8) or 1
+            try:
+                return value.to_bytes(width_, byteorder='big', signed=signed)
+            except (OverflowError):
+                return value.to_bytes(width_ + 1, byteorder='big', signed=signed)
+        else:
+            return value.to_bytes(width_, byteorder='big', signed=signed)
 
     def int_from_bytes(value, signed=False):
         """
@@ -109,7 +137,8 @@ else:
 
 def fill_width(bytes_, width):
     """
-    Ensure a byte string representing an integer is a specific width (in bytes)
+    Ensure a byte string representing a positive integer is a specific width
+    (in bytes)
 
     :param bytes_:
         The integer byte string
