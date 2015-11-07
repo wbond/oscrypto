016f4b76398bb9dab52699610779d974a17e81d7
wbond <will@wbond.net>
2015-11-07 07:53:36 -0500

    Try to get more info on OS X when there is a protocol error

--- oscrypto/_osx/tls.py
+++ oscrypto/_osx/tls.py
@@ -145,6 +145,30 @@ def _read_callback(connection_id, data_buffer, data_length_pointer):
     return 0
 
 
+def _read_remaining(socket):
+    """
+    Reads everything available from the socket - used for debugging when there
+    is a protocol error
+
+    :param socket:
+        The socket to read from
+
+    :return:
+        A byte string of the remaining data
+    """
+
+    output = b''
+    old_timeout = socket.gettimeout()
+    try:
+        socket.settimeout(0.0)
+        output += socket.recv(8192)
+    except (socket_.error):
+        pass
+    finally:
+        socket.settimeout(old_timeout)
+    return output
+
+
 def _write_callback(connection_id, data_buffer, data_length_pointer):
     """
     Callback called by Secure Transport to actually write to the socket
@@ -705,9 +729,12 @@ class TLSSocket(object):
                     raise_dh_params()
 
             if handshake_result in set([SecurityConst.errSSLRecordOverflow, SecurityConst.errSSLProtocol]):
+                self._server_hello += _read_remaining(self._socket)
                 raise_protocol_error(self._server_hello)
 
             if handshake_result in set([SecurityConst.errSSLClosedNoNotify, SecurityConst.errSSLClosedAbort]):
+                if not self._done_handshake:
+                    self._server_hello += _read_remaining(self._socket)
                 if detect_other_protocol(self._server_hello):
                     raise_protocol_error(self._server_hello)
                 raise_disconnection()
