67d0017957ef024e18cbe5522e365771ed1b0e94
wbond <will@wbond.net>
2019-07-23 06:54:31 -0400

    Fix aes_cbc_no_padding_encrypt() with OpenSSL, add tests

--- tests/test_symmetric.py
+++ tests/test_symmetric.py
@@ -7,6 +7,7 @@ import sys
 from oscrypto import symmetric, util
 
 from ._unittest_compat import patch
+from .exception_context import assert_exception
 
 patch()
 
@@ -17,6 +18,32 @@ else:
 
 
 class SymmetricTests(unittest.TestCase):
+
+    def test_aes_cbc_no_padding_encrypt_decrypt(self):
+        key = util.rand_bytes(16)
+        data = b'This is data to encrypt-32 bytes'
+
+        iv, ciphertext = symmetric.aes_cbc_no_padding_encrypt(key, data, None)
+        self.assertNotEqual(data, ciphertext)
+        self.assertEqual(byte_cls, type(ciphertext))
+
+        plaintext = symmetric.aes_cbc_no_padding_decrypt(key, ciphertext, iv)
+        self.assertEqual(data, plaintext)
+
+    def test_aes_cbc_no_padding_wrong_length(self):
+        key = util.rand_bytes(16)
+
+        with assert_exception(self, ValueError, r'data must be a multiple of 16 bytes long - is 31'):
+            data = b'31 bytes of data to encrypt now'
+            iv, ciphertext = symmetric.aes_cbc_no_padding_encrypt(key, data, None)
+
+        with assert_exception(self, ValueError, r'data must be a multiple of 16 bytes long - is 33'):
+            data = b'Thirty three bytes to encrypt now'
+            iv, ciphertext = symmetric.aes_cbc_no_padding_encrypt(key, data, None)
+
+        with assert_exception(self, ValueError, r'data must be a multiple of 16 bytes long - is 15'):
+            data = b'Fifteen bytes!!'
+            iv, ciphertext = symmetric.aes_cbc_no_padding_encrypt(key, data, None)
 
     def test_aes_128_encrypt_decrypt(self):
         key = util.rand_bytes(16)
