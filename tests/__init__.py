599305e5acae0fed490fbe5a445d41809c898805
wbond <will@wbond.net>
2022-03-08 21:10:36 -0500

    Tweak construction for skipping TLS tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -138,18 +138,18 @@ def test_classes():
     from .test_asymmetric import AsymmetricTests
     from .test_symmetric import SymmetricTests
     from .test_trust_list import TrustListTests
-    if not os.environ.get('OSCRYPTO_SKIP_INTERNET_TESTS'):
-        from .test_tls import TLSTests
-    else:
-        TLSTests = None
     from .test_init import InitTests
 
-    return filter(None, [
+    test_classes = [
         KDFTests,
         KeyTests,
         AsymmetricTests,
         SymmetricTests,
         TrustListTests,
-        TLSTests,
         InitTests,
-    ])
+    ]
+    if not os.environ.get('OSCRYPTO_SKIP_INTERNET_TESTS'):
+        from .test_tls import TLSTests
+        test_classes.append(TLSTests)
+
+    return test_classes
