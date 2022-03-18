7a8906149d6ed898f2622c627dee342e9e87a34f
wbond <will@wbond.net>
2022-03-17 21:24:53 -0400

    Improve exhaustiveness of tests for AES symmetric encryption

--- tests/test_symmetric.py
+++ tests/test_symmetric.py
@@ -8,6 +8,7 @@ from oscrypto import symmetric, util, backend
 
 from ._unittest_compat import patch
 from .exception_context import assert_exception
+from .unittest_data import data_decorator, data
 
 patch()
 
@@ -27,10 +28,33 @@ else:
     supports_legacy = True
 
 
+@data_decorator
 class SymmetricTests(unittest.TestCase):
 
-    def test_aes_cbc_no_padding_encrypt_decrypt(self):
-        key = util.rand_bytes(16)
+    @staticmethod
+    def aes_key_lengths():
+        return (
+            ('128', 16),
+            ('192', 24),
+            ('256', 32),
+        )
+
+    @data('aes_key_lengths', True)
+    def aes_cbc_no_padding_encrypt_decrypt(self, key_byte_len):
+        key = util.rand_bytes(key_byte_len)
+        data = b'This is the data'
+
+        iv, ciphertext = symmetric.aes_cbc_no_padding_encrypt(key, data, None)
+        self.assertNotEqual(data, ciphertext)
+        self.assertEqual(byte_cls, type(ciphertext))
+
+        plaintext = symmetric.aes_cbc_no_padding_decrypt(key, ciphertext, iv)
+        self.assertEqual(data, plaintext)
+
+    @data('aes_key_lengths', True)
+    def aes_cbc_no_padding_encrypt_decrypt_two_block(self, key_byte_len):
+        key = util.rand_bytes(key_byte_len)
+
         data = b'This is data to encrypt-32 bytes'
 
         iv, ciphertext = symmetric.aes_cbc_no_padding_encrypt(key, data, None)
@@ -40,30 +64,9 @@ class SymmetricTests(unittest.TestCase):
         plaintext = symmetric.aes_cbc_no_padding_decrypt(key, ciphertext, iv)
         self.assertEqual(data, plaintext)
 
-    def test_aes_192_cbc_no_padding_encrypt_decrypt(self):
-        key = util.rand_bytes(24)
-        data = b'This is the data'
-
-        iv, ciphertext = symmetric.aes_cbc_no_padding_encrypt(key, data, None)
-        self.assertNotEqual(data, ciphertext)
-        self.assertEqual(byte_cls, type(ciphertext))
-
-        plaintext = symmetric.aes_cbc_no_padding_decrypt(key, ciphertext, iv)
-        self.assertEqual(data, plaintext)
-
-    def test_aes_256_cbc_no_padding_encrypt_decrypt(self):
-        key = util.rand_bytes(32)
-        data = b'This is the data'
-
-        iv, ciphertext = symmetric.aes_cbc_no_padding_encrypt(key, data, None)
-        self.assertNotEqual(data, ciphertext)
-        self.assertEqual(byte_cls, type(ciphertext))
-
-        plaintext = symmetric.aes_cbc_no_padding_decrypt(key, ciphertext, iv)
-        self.assertEqual(data, plaintext)
-
-    def test_aes_cbc_no_padding_wrong_length(self):
-        key = util.rand_bytes(16)
+    @data('aes_key_lengths', True)
+    def aes_cbc_no_padding_wrong_length(self, key_byte_len):
+        key = util.rand_bytes(key_byte_len)
 
         with assert_exception(self, ValueError, r'data must be a multiple of 16 bytes long - is 31'):
             data = b'31 bytes of data to encrypt now'
@@ -77,19 +80,13 @@ class SymmetricTests(unittest.TestCase):
             data = b'Fifteen bytes!!'
             iv, ciphertext = symmetric.aes_cbc_no_padding_encrypt(key, data, None)
 
-    def test_aes_128_encrypt_decrypt(self):
-        key = util.rand_bytes(16)
-        data = b'This is data to encrypt'
+        with assert_exception(self, ValueError, r'data must be a multiple of 16 bytes long - is 24'):
+            data = b'Twenty four bytes long!!'
+            iv, ciphertext = symmetric.aes_cbc_no_padding_encrypt(key, data, None)
 
-        iv, ciphertext = symmetric.aes_cbc_pkcs7_encrypt(key, data, None)
-        self.assertNotEqual(data, ciphertext)
-        self.assertEqual(byte_cls, type(ciphertext))
-
-        plaintext = symmetric.aes_cbc_pkcs7_decrypt(key, ciphertext, iv)
-        self.assertEqual(data, plaintext)
-
-    def test_aes_256_encrypt_decrypt(self):
-        key = util.rand_bytes(32)
+    @data('aes_key_lengths', True)
+    def aes_encrypt_decrypt(self, key_byte_len):
+        key = util.rand_bytes(key_byte_len)
         data = b'This is data to encrypt'
 
         iv, ciphertext = symmetric.aes_cbc_pkcs7_encrypt(key, data, None)
